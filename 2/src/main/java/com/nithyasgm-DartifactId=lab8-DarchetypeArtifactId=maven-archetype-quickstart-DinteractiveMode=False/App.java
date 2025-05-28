b9995e46e24c728f4840526fa9e735255bcc135d
nithyasgn <nshree80562@gmail.com>
2025-05-28 07:40:29 +0530

    first commit

---
+++ 2/src/main/java/com/nithyasgm-DartifactId=lab8-DarchetypeArtifactId=maven-archetype-quickstart-DinteractiveMode=False/App.java
@@ -0,0 +1,10 @@
+package com.nithyasgm-DartifactId=lab8-DarchetypeArtifactId=maven-archetype-quickstart-DinteractiveMode=False;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
